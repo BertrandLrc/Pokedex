ceea680512acff2cb70ebb4fa6ba5ca0f0e3e283
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-04-23 16:46:21 +0200

    components 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import PokemonCard from "./components/Salameche";
+import PokemonCard from "./components/PokemonCard.jsx";
 
 function App() {
   return (
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,30 @@
+import styles from './PokemonCard.herbe.module.css'
+
+const pokemonList = [
+  {
+    name: "Bulbizarre",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
+function PokemonCard() {
+  const pokemon = pokemonList[1];
+
+  return (
+  <figure className={styles.card}>
+    {pokemon.hasOwnProperty(`imgSrc`) ? (
+      <img src={pokemon.imgSrc} alt={pokemon.name} className={styles.cardImg} />
+    ) : (
+      <p>???</p>
+    )}
+  <figcaption>{pokemon.name}</figcaption>
+  </figure>
+    );
+  }
+
+
+export default PokemonCard;
