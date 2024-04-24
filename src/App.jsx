edd89d17fdfcde9053fcf6b6887df4ccc3165614
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-04-24 15:19:45 +0200

    props.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import PokemonCard from "./components/PokemonCard.jsx";
 
+const pokemonList = [
+  {
+    name: "Bulbizarre",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   return (
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]}/>
   );
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,30 +1,18 @@
 import styles from './PokemonCard.herbe.module.css'
 
-const pokemonList = [
-  {
-    name: "Bulbizarre",
-    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
-
-function PokemonCard() {
-  const pokemon = pokemonList[1];
+function PokemonCard({pokemon}) {
 
   return (
-  <figure className={styles.card}>
-    {pokemon.hasOwnProperty(`imgSrc`) ? (
-      <img src={pokemon.imgSrc} alt={pokemon.name} className={styles.cardImg} />
-    ) : (
-      <p>???</p>
-    )}
-  <figcaption>{pokemon.name}</figcaption>
-  </figure>
-    );
-  }
-
+    <figure className={styles.card}>
+      {(pokemon.imgSrc !== undefined) ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} className={styles.cardImg} />
+       ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
 
 export default PokemonCard;
